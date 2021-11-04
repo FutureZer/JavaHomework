3e049fb083652468b80d63e5368d98cb3cc5c5eb
FutuZer <71083207+FutureZer@users.noreply.github.com>
2021-11-04 17:08:30 +0300

    Update Fileiterator.java

--- Homework_4/src/ru/hse/cs/bpi208/homework_4/Fileiterator.java
+++ Homework_4/src/ru/hse/cs/bpi208/homework_4/Fileiterator.java
@@ -3,14 +3,16 @@ package ru.hse.cs.bpi208.homework_4;
 import java.io.*;
 import java.util.Iterator;
 
-public class Fileiterator implements Iterator {
+public class Fileiterator implements Iterator<String>, Closeable {
 
     private String currentLine;
     private final BufferedReader reader;
+    private final FileReader fr;
+    private final File file;
 
     public Fileiterator(String path) throws IOException {
-        File file = new File(path);
-        FileReader fr = new FileReader(file);
+        file = new File(path);
+        fr = new FileReader(file);
         reader = new BufferedReader(fr);
         currentLine = reader.readLine();
     }
@@ -23,12 +25,23 @@ public class Fileiterator implements Iterator {
     @Override
     public String next() {
         try {
+            if (hasNext()) {
+                throw new NullPointerException("Итератор дошел до конца файла");
+            }
             String result = currentLine;
             currentLine = reader.readLine();
             return result;
+        } catch (NullPointerException ex) {
+            throw ex;
         } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
+            throw new UncheckedIOException("Возникла неизвестная проблема при работе с файлом", ex);
         }
+    }
+
+    @Override
+    public void close() throws IOException {
+        file.delete();
+        fr.close();
+        reader.close();
     }
 }
